fbe5b9b176f50699565d0d56307273ff4d3324dc
Carlos Agustin <carlos.agustin218@gmail.com>
2021-07-06 15:34:07 -0700

    adding work to be done

--- monolithicProgram.cpp
+++ monolithicProgram.cpp
@@ -1,19 +1,19 @@
+// Example program
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
-    int length{0}, breadth{0};
-     
-    cout << "Enter length and Breadth";
-    cin >> length >> breadth;
-
+int main()
+{
+    int length = 0, breadth =0;
+    cout << "Enter Length and Breadth";
+    cin >> length>> breadth;
+    
     int area = length * breadth;
-
-    int peri = 2 * ( length + breadth);
-
+    int perimeter = 2 * ( length + breadth);
+    
     cout << "Area = " << area << endl
-        << "Perimeter = " << peri << endl;
-
-
+        << "Perimeter = " << perimeter << endl;
+        
     return 0;
 }
